b10ffe5acc52e0ebe1f4b7e3fbf2c1bf517da9ae
Eddie Arnold <eddiearnoldz@me.com>
2022-03-20 19:16:45 +0000

    added access rules for when token is valid and routes to send when user not logged in

--- client_side/src/App.js
+++ client_side/src/App.js
@@ -1,25 +1,48 @@
-import React from 'react'
+import React,{useEffect,createContext, useReducer, useContext} from 'react'
 import Navbar from './components/navbar'
 import "./App.css"
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import {BrowserRouter, Route, Routes, useNavigate} from 'react-router-dom'
 import Home from './components/screens/Home'
 import Profile from './components/screens/Profile'
 import SignIn from './components/screens/SignIn'
 import SignUp from './components/screens/SignUp'
 import CreatePost from './components/screens/CreatePost'
+import {initialState, reducer} from './reducers/userReducer'
+
+export const UserContext = createContext()
+const Routing = () => {
+  const navigate = useNavigate()
+  const {state, dispatch} = useContext(UserContext)
+  useEffect(()=>{
+    const user = JSON.parse(localStorage.getItem("user"))
+
+    if(user){
+      dispatch({type:"USER", payload:user})
+      navigate('/')
+    }else{
+      navigate('/signin')
+    }
+  }, [])
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/signin" element={<SignIn />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/createpost" element={<CreatePost />} />
+    </Routes>
+    )
+}
 
 function App() {
+const [state,dispatch] = useReducer(reducer,initialState)
   return (
+    <UserContext.Provider value = {{state,dispatch}}>
     <BrowserRouter>
     <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/createpost" element={<CreatePost />} />
-      </Routes>
+      <Routing/>
     </BrowserRouter>
+    </UserContext.Provider>
   ); 
 }
 
--- client_side/src/components/screens/SignIn.js
+++ client_side/src/components/screens/SignIn.js
@@ -1,8 +1,10 @@
-import React,{useState} from 'react'
+import React,{useState, useContext,} from 'react'
 import {Link, useNavigate} from 'react-router-dom'
+import { UserContext } from '../../App'
 import M from 'materialize-css'
 
 const SignIn = () => {
+  const {state, dispatch} = useContext(UserContext)
   const navigation = useNavigate()
   const [password,setPassword] = useState("")
   const [email,setEmail] = useState("")
@@ -28,6 +30,7 @@ const SignIn = () => {
        } else {
          localStorage.setItem("jwt",data.token)
          localStorage.setItem("user",JSON.stringify(data.user))
+         dispatch({type:"USER", payload:data.user})
          M.toast({html:"signed in successfuly", classes: '#536dfe indigo accent-2'})
          navigation('/')
        } 
